e554a1801ee1005dca7d7aeba63b0d20339b05ee
Sam <sayyamo56@gmail.com>
2026-08-21 12:31:55 +0530

    spacing

--- parasmani-catalog/src/components/Collections/Collections.jsx
+++ parasmani-catalog/src/components/Collections/Collections.jsx
@@ -24,7 +24,7 @@ function Collections() {
   };
 
   return (
-    <section className="py-16 md:py-24 bg-white">
+    <section className="py-10 md:py-14 bg-white">
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
@@ -39,7 +39,7 @@ function Collections() {
           Our Collections
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8 mt-12 md:mt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8 mt-8 md:mt-10">
 
           {featuredCategories.map((category) => (
 
